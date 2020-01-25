319388665aebda717fea54d8501907b94eadc11f
MrFORBIDEN <54280101+MrFORBIDEN@users.noreply.github.com>
2020-01-25 12:57:00 +0700

    Create fbr.py

---
+++ fbr.py
@@ -0,0 +1,22 @@
+@@ -45,11 +45,11 @@ def prBlack(skk): print("\033[98m {}\033[00m" .format(skk))
+           |_| |_.__/|_|    @New Update 0.3
+              
+          [FBR Facebook Hacking Tool]
+        [@Created By Cabdualahi Sharif]      """+C+p+"""
+             Youtube Channel: Somali4You
+             Facebook: cabdulahi.sharif.100 ------------------------------------------"""
+        [@Created By Hanif Marri] DAVID    """+C+p+"""
+             Youtube Channel: Technical Hanif
+             Facebook: m.facebook.com/hanif.marri------------------------------------------"""
+print banner
+me = (C+p+'[💡]Cabdulahi=>> ')
+me = (C+p+'[💡] Options=>> ')
+oh= time.sleep(1)
+
+
+ @@ -122,4 +122,4 @@ def updown(self):
+
+
+ if __name__ == "__main__":
+	fbr() 
+	fbr()
